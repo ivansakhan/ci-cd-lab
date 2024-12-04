faa067502ddba8f7cdf258d320b1f3d3fd63c6cf
ivansakhan <ivan.sakhan@gmail.com>
2024-12-04 19:09:59 +0000

    fixed reverseString

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 function reverseString(input) {
-  if (typeof input !== "string") {
-      throw new Error("Input must be a string");
-  }
-  return input.split("").reverse().join("");
+    if (typeof input !== "string") {
+        throw new Error("Input must be a string");
+    }
+    return input.split("").reverse().join("");
 }
 
-// Тестовий запуск
-const testString = "hello";
-console.log(`Input: ${testString}, Reversed: ${reverseString(testString)}`);
+// Експортуємо функцію
+module.exports = reverseString;
